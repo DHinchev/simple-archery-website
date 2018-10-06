97da33dfbed25e4d3d9f26a2f36142651d671d02
Daniel <d.r.hinchev@gmail.com>
2018-10-07 01:11:12 +0300

    format code and tidy up

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,11 @@ require(['jquery', 'slider']);
         const menuIcon = document.querySelector('.menu-icon');
         const menuSlider = document.querySelector('.slide-menu');
 
-        function addClassFunFive() {
-            menuSlider.classList.toggle("open");
+        function slideMenu() {
+            menuSlider.classList.toggle('open');
         }
 
-        menuIcon.addEventListener('click', addClassFunFive);
+        menuIcon.addEventListener('click', slideMenu);
 
         function initMap() {
             var shootingRange = {
@@ -16,7 +16,7 @@ require(['jquery', 'slider']);
                 lng: 23.360065
             };
             var map = new google.maps.Map(
-                document.getElementById('location-container'), {
+                document.querySelector('.location-container'), {
                     zoom: 14,
                     center: shootingRange
                 });
@@ -29,8 +29,8 @@ require(['jquery', 'slider']);
         const anchorLinkHandler = (e) => {
             const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
             e.preventDefault();
-            const element = (e.target.nodeName == 'svg') ? e.target.parentElement : e.target;
-            const targetID = element.getAttribute("href");
+            const element = e.target;
+            const targetID = element.getAttribute('href');
             const targetAnchor = document.querySelector(targetID);
             if (!targetAnchor) return;
             const originalTop = distanceToTop(targetAnchor);
@@ -38,15 +38,15 @@ require(['jquery', 'slider']);
             window.scrollBy({
                 top: originalTop,
                 left: 0,
-                behavior: "smooth"
+                behavior: 'smooth'
             });
 
             const checkIfDone = setInterval(() => {
                 const bottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
                 if (distanceToTop(targetAnchor) === 0 || bottom) {
-                    targetAnchor.tabIndex = "-1";
+                    targetAnchor.tabIndex = '-1';
                     targetAnchor.focus();
-                    window.history.pushState("", "", targetID);
+                    window.history.pushState('', '', targetID);
                     clearInterval(checkIfDone);
                 }
             }, 100);
